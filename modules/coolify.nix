a5548318aaffc55bbdb08701989c633c4e185477
Raphaël Catarino <raphcatarino@gmail.com>
2024-06-14 15:39:50 +0200

    fix curl

--- modules/coolify.nix
+++ modules/coolify.nix
@@ -22,10 +22,10 @@ in
         fi
       done
 
-      curl -fsSL https://cdn.coollabs.io/coolify/docker-compose.yml -o /data/coolify/source/docker-compose.yml
-      curl -fsSL https://cdn.coollabs.io/coolify/docker-compose.prod.yml -o /data/coolify/source/docker-compose.prod.yml
-      curl -fsSL https://cdn.coollabs.io/coolify/.env.production -o /data/coolify/source/.env
-      curl -fsSL https://cdn.coollabs.io/coolify/upgrade.sh -o /data/coolify/source/upgrade.sh
+      ${pkgs.curl} -fsSL https://cdn.coollabs.io/coolify/docker-compose.yml -o /data/coolify/source/docker-compose.yml
+      ${pkgs.curl} -fsSL https://cdn.coollabs.io/coolify/docker-compose.prod.yml -o /data/coolify/source/docker-compose.prod.yml
+      ${pkgs.curl} -fsSL https://cdn.coollabs.io/coolify/.env.production -o /data/coolify/source/.env
+      ${pkgs.curl} -fsSL https://cdn.coollabs.io/coolify/upgrade.sh -o /data/coolify/source/upgrade.sh
 
       # Generate SSH key if not ready
       if [ ! -f "/data/coolify/ssh/keys/id.root@host.docker.internal" ]; then
