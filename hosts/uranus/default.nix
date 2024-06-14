fb609d8ec8de425570f3de4d005d5ad8a9f4dd4b
Raphaël Catarino <raphcatarino@gmail.com>
2024-06-14 15:06:27 +0200

    fix path

--- hosts/uranus/default.nix
+++ hosts/uranus/default.nix
@@ -2,8 +2,8 @@
 
 {
   imports = [
-    (builtins.toFile /etc/nixos/hosts/uranus/hardware-configuration.nix)
-    (builtins.toFile /etc/nixos/hosts/uranus/networking.nix)
+    (builtins.toFile "/etc/nixos/hosts/uranus/hardware-configuration.nix")
+    (builtins.toFile "/etc/nixos/hosts/uranus/networking.nix")
     ../../modules/system.nix
     ../../modules/docker.nix
     ../../modules/tailscale.nix
