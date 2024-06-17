5584b9399cca06be8c4aebaf93529867da2d14ed
Raphaël Catarino <raphcatarino@gmail.com>
2024-06-17 20:38:48 +0200

    fix docker config

--- modules/docker.nix
+++ modules/docker.nix
@@ -6,11 +6,12 @@
 
   home-manager.users.${globals.username}.home.file.dockerConfig = {
     source = ../secrets/docker.json;
-    target = ".docker/config.json";
+    target = "./.tmp/.docker/config.json";
     onChange = ''
-      cp  .docker/config.json .docker/config.json.tmp
-      rm -f .docker/config.json
-      mv .docker/config.json.tmp .docker/config.json
+      mkdir -p ./.docker
+      rm -rf ./.docker/config.json
+      cp -rL ./.tmp/.docker/config.json ./.docker/config.json
+      rm -rf ./.tmp/.docker/config.json
     '';
   };
 }
