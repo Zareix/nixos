6f9650243471149969baf072dde997c80d6f59b7
Raphaël Catarino <raphcatarino@gmail.com>
2024-06-17 22:26:21 +0200

    fix params

--- modules/share-server.nix
+++ modules/share-server.nix
@@ -1,9 +1,9 @@
-{ globals, config, ... }:
+{ globals, shareFolder, ... }:
 {
   # NFS
   services.nfs.server.enable = true;
   services.nfs.server.exports = ''
-    ${config.shareFolder} *(rw,insecure,sync,no_subtree_check,all_squash,anonuid=1000,anongid=1000,fsid=0)
+    ${shareFolder} *(rw,insecure,sync,no_subtree_check,all_squash,anonuid=1000,anongid=1000,fsid=0)
   '';
 
   # SAMBA
@@ -13,7 +13,7 @@
     # openFirewall = true;
     shares = {
       share = {
-        path = config.shareFolder;
+        path = shareFolder;
         browseable = "yes";
         "read only" = "no";
         "guest ok" = "no";
