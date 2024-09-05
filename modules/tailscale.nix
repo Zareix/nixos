8c9da1d2d8ab433999faa2baa1168288fcfff8cb
System administrator <root@jupiter.taile4fbf.ts.net>
2024-09-05 13:25:59 +0200

    remove dns internal

--- modules/tailscale.nix
+++ modules/tailscale.nix
@@ -2,4 +2,10 @@
   services.tailscale.enable = true;
   services.tailscale.package = pkgs-unstable.tailscale;
   services.tailscale.extraUpFlags = [ "--stateful-filtering=false" ];
+
+  services.resolved = {
+    extraConfig = ''
+      DNSStubListener=no
+    '';
+  };
 }
