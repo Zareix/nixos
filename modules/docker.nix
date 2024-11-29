71edebccda31df6a17700108fbbd031285515cff
Raphaël Catarino <raphcatarino@gmail.com>
2024-11-29 22:09:05 +0100

    update

--- modules/docker.nix
+++ modules/docker.nix
@@ -7,4 +7,11 @@
   virtualisation.docker.package = pkgs.docker_27;
 
   users.users.${globals.username}.extraGroups = ["docker"];
+
+  services.gitwatch.docker-stacks = {
+    enable = true;
+    path = "/data/stacks";
+    remote = "https://github.com/Zareix/docker.git";
+    user = "[Gitwatch] Zareix";
+  };
 }
