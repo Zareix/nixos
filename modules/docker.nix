c234714d92883bf1eb68cbf873bd188c8aec3a8a
Raphaël Catarino <raphcatarino@gmail.com>
2025-05-28 22:54:10 +0200

    update

--- modules/docker.nix
+++ modules/docker.nix
@@ -2,9 +2,11 @@
   globals,
   pkgs,
   ...
-}: {
+}: let
+  dockerPkg = pkgs.docker_28;
+in {
   virtualisation.docker.enable = true;
-  virtualisation.docker.package = pkgs.docker_27;
+  virtualisation.docker.package = dockerPkg;
 
   users.users.${globals.username}.extraGroups = ["docker"];
 
@@ -13,5 +15,12 @@
     path = "/data/stacks";
     remote = "https://github.com/Zareix/docker.git";
     user = "raphaelgc";
+  };
+
+  services.cron = {
+    enable = true;
+    systemCronJobs = [
+      "30 9 * * * root echo \"$(date): Starting docker prune\" >> /tmp/cron_docker-prune.log && ${dockerPkg}/bin/docker system prune -af >> /tmp/cron_docker-prune.log 2>&1"
+    ];
   };
 }
