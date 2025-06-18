b75f99073f89a9b71a3b5a12eeae36ea7f1e65d8
Raphaël Catarino <raphcatarino@gmail.com>
2025-06-18 15:30:50 +0200

    added luna

--- hosts/luna/default.nix
+++ hosts/luna/default.nix
@@ -1,5 +1,6 @@
-{
+{pkgs, ...}: {
   imports = [
+    pkgs.modules.installer.cd-dvd.installation-cd-minimal-combined
     ../../modules/home-manager.nix
     ../../modules/system.nix
     ../../modules/docker.nix
